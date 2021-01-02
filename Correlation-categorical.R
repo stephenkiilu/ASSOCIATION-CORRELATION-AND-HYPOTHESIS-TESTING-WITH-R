3dcb451fa861c0ba97b10e04d49bee22e28b97b4
Stephen Kiilu <76428853+stephenkiilu@users.noreply.github.com>
2021-01-02 15:43:47 +0200

    Add files via upload

---
+++ Correlation-categorical.R
@@ -0,0 +1,44 @@
+
+
+
+data_income_class <- read.csv("~/R/Udemy/data_income_class.csv")
+View(data_income_class)
+
+table(data_income_class$gender,data_income_class$income)
+chisq.test(data_income_class$gender,data_income_class$income)
+table(data_income_class$education,data_income_class$income)
+chisq.test(data_income_class$education,data_income_class$income)
+ barplot(table(data_income_class$income,data_income_class$education),
+         las=1,horiz = T,legend.text = T) 
+ 
+5=data_mcharges_extended
+male=charges[charges$sex=="male",3]
+female=charges[charges$sex=="female",3]
+t.test(male,female)
+
+### Normality
+shapiro.test(male )
+shapiro.test(female )
+shapiro.test(log(female ))
+#### Homogeneity
+bartlett.test(charges$charges~charges$sex)
+
+##t-test
+t.test(male,female)
+### Region
+table(charges$region)#### we cant use t-test
+##ANOVA
+##Normality test of 3 regions
+east=charges[charges$region=="east",3]
+nw=charges[charges$region=="northwest",3]
+sw=charges[charges$region=="souththwest",3]
+shapiro.test(east)
+shapiro.test(nw)
+shapiro.test(sw)
+
+##Homogeneity
+bartlett.test(charges$charges~charges$region)
+#ANOVA
+fit=aov(charges$charges~charges$region)
+summary(fit)
+             
